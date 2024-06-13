0bcdf974abaadb7cf3fd8e59bcd670d58743f00f
GlebShevardin <shevardingleb10@gmail.com>
2024-06-13 13:26:36 +0300

    solved '[1] use strict .js'

--- Module 1/[1] use strict.js
+++ Module 1/[1] use strict.js
@@ -3,3 +3,17 @@
 качестве аргумента и возвращать его
 факториал. Учтите использование строгого
 режима */
+
+'use strict';
+
+function findFactorial(num) {
+    let factorial = 1;
+
+    for(let i = num; i > 0; i--){
+        factorial *= i
+    }
+
+    return factorial
+}
+
+console.log(findFactorial(6))
